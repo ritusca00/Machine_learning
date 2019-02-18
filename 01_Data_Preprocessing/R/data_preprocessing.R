2c254b846be17da6ce0d06d1002b817a3f95b695
ritusca00 <bliznikrita@hotmail.com>
2019-02-18 19:28:46 +0100

    Categorical_data and splitting dataset
    Categorical data and splitting the dataset into the training set and test set

--- 01_Data_Preprocessing/R/data_preprocessing.R
+++ 01_Data_Preprocessing/R/data_preprocessing.R
@@ -8,4 +8,17 @@
                          ave(dataset$Salary, FUN=function(x) mean(x, na.rm = TRUE)),
                          dataset$Salary)
  
+ dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                          labels = c(1, 2, 3))
+  
+ dataset$Purchased = factor(dataset$Purchased,
+                          levels = c('No', 'Yes'),
+                          labels = c(0,1))
+ 
+ library(caTools)
+ set.seed(123)
+ split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+ training_set = subset(dataset, split == TRUE)
+ test_set = subset(dataset, split == FALSE)
  
